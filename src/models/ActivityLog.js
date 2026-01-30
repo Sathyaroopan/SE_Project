9213160986ec40d60116833f7722e11468d01f08
Yoganand242 <154869659+Yoganand242@users.noreply.github.com>
2026-01-30 11:22:47 +0530

    ActivityLog.js

--- src/models/ActivityLog.js
+++ src/models/ActivityLog.js
@@ -1,1 +1,34 @@
+import mongoose from "mongoose";
+
+const ActivityLogSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    taskId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+    },
+    type: {
+      type: String,
+      enum: [
+        "task_delay",
+        "missed_session",
+        "session_completed",
+        "deadline_missed",
+      ],
+      required: true,
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.ActivityLog ||
+  mongoose.model("ActivityLog", ActivityLogSchema);
 
