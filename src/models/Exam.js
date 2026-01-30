df7903a08381ce49a1d23097a0721f2046e8dc78
Yoganand242 <154869659+Yoganand242@users.noreply.github.com>
2026-01-30 11:21:55 +0530

    Exam.js

--- src/models/Exam.js
+++ src/models/Exam.js
@@ -1,1 +1,31 @@
+import mongoose from "mongoose";
+
+const ExamSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    courseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    examDate: {
+      type: Date,
+      required: true,
+    },
+    syllabusWeight: {
+      type: Number,
+      min: 0,
+      max: 100,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Exam ||
+  mongoose.model("Exam", ExamSchema);
 
