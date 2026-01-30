117a9de727076d930e17acd980b11e2316f64180
Yoganand242 <154869659+Yoganand242@users.noreply.github.com>
2026-01-30 11:22:24 +0530

    Studysession.js

--- src/models/Studysession.js
+++ src/models/Studysession.js
@@ -1,1 +1,37 @@
+import mongoose from "mongoose";
+
+const StudySessionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    taskId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+      required: true,
+    },
+    startTime: {
+      type: Date,
+      required: true,
+    },
+    endTime: {
+      type: Date,
+      required: true,
+    },
+    actualDuration: {
+      type: Number, // in minutes
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.StudySession ||
+  mongoose.model("StudySession", StudySessionSchema);
 
