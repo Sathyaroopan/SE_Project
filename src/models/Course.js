eacd7942fb3043763f903ca507d024445b7fc26b
Yoganand242 <154869659+Yoganand242@users.noreply.github.com>
2026-01-30 11:20:49 +0530

    Course.js

--- src/models/Course.js
+++ src/models/Course.js
@@ -1,1 +1,34 @@
+import mongoose from "mongoose";
+
+const CourseSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    semester: {
+      type: String,
+      required: true,
+    },
+    credit: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    difficulty: {
+      type: String,
+      enum: ["easy", "medium", "hard"],
+      default: "medium",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Course ||
+  mongoose.model("Course", CourseSchema);
 
